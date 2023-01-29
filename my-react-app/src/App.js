0596772b8ff00d695d3374df706e829ba79e01f8
SenFaruk <kiowa297@gmail.com>
2023-01-29 13:15:13 +0300

    Route finished

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,121 @@
-import Event from "./components/Event";
-import Header from "./components/Header";
-import MapFunction from "./components/MapFunction";
-import ProductList from "./components/ProductList";
+// import Event from "./components/Event";
+// import Header from "./components/Header";
+// import MapFunction from "./components/MapFunction";
+// import ProductList from "./components/ProductList";
 
-import UseStateHook from "./components/UseStateHook";
-import React, { useState } from "react";
+// import UseStateHook from "./components/UseStateHook";
+// import React, { useState } from "react";
+
+// function App() {
+//   const clickMe = (adın) => {
+//     console.log("ismin:" + adın);
+//   };
+
+//   const [products, setProducts] = useState([
+//     { id: 1, title: "Product 1", price: 899 },
+//     { id: 2, title: "Product 2", price: 982 },
+//     { id: 3, title: "Product 3", price: 322 },
+//     { id: 4, title: "Product 4", price: 763 },
+//     { id: 5, title: "Product 5", price: 389 },
+//   ]);
+
+//   return (
+//     <>
+//       {/* <div className="App">
+//   <Header/>
+//   <button onClick={()=>clickMe("faruk")}>clickme</button>
+//     </div> */}
+//       {/* <Event/> */}
+//       {/* <UseStateHook/> */}
+//       {/* <MapFunction/> */}
+//       <ProductList products={products} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// ===============================00
+
+// import { useState,useEffect } from "react"
+// import ProductList from "./components/ProductList";
+
+// function App() {
+//   const [products, setProducts] = useState([
+//     {id: 1, title: 'Product 1', price: 899},
+//     {id: 2, title: 'Product 2', price: 982},
+//     {id: 3, title: 'Product 3', price: 322},
+//     {id: 4, title: 'Product 4', price: 763},
+//     {id: 5, title: 'Product 5', price: 389}
+//   ]);
+
+//   const deleteProduct = (productId) => {
+//     const newProducts = products.filter(product => product.id !== productId);
+//     setProducts(newProducts);
+//   }
+
+//   useEffect(() => {
+//     console.log('Use Effect Running');
+//   }, []);
+
+//   return (
+//     <div>
+//       <ProductList products={ products } deleteProduct={ deleteProduct } />
+//     </div>
+//   );
+// }
+
+// export default App;
+// ======================
+
+// herhangibir durum değişikliğindede useEffect kullanıla bilir
+// şöyleki
+
+// import { useState, useEffect } from "react"
+// import ProductList from "./components/ProductList";
+
+// function App() {
+//   const [products, setProducts] = useState([
+//     {id: 1, title: 'Product 1', price: 899},
+//     {id: 2, title: 'Product 2', price: 982},
+//     {id: 3, title: 'Product 3', price: 322},
+//     {id: 4, title: 'Product 4', price: 763},
+//     {id: 5, title: 'Product 5', price: 389}
+//   ]);
+
+//   const [name, setName] = useState('Fikri');
+
+//   const deleteProduct = (productId) => {
+//     const newProducts = products.filter(product => product.id !== productId);
+//     setProducts(newProducts);
+//   }
+
+//   useEffect(() => {
+//     console.log('Use Effect Running');
+//   }, [name]);
+
+//   return (
+//     <div>
+//       <ProductList products={ products } deleteProduct={ deleteProduct } />
+//       <button onClick={ () => setName('John') }>Change Name</button>
+//       <p>Name: { name }</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ================
+
+// #10. React Router
+
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-  const clickMe = (adın) => {
-    console.log("ismin:" + adın);
-  };
-
   const [products, setProducts] = useState([
     { id: 1, title: "Product 1", price: 899 },
     { id: 2, title: "Product 2", price: 982 },
@@ -19,17 +124,27 @@ function App() {
     { id: 5, title: "Product 5", price: 389 },
   ]);
 
+  const deleteProduct = (productId) => {
+    const newProducts = products.filter((product) => product.id !== productId);
+    setProducts(newProducts);
+  };
+
   return (
-    <>
-      {/* <div className="App">
-  <Header/>
-  <button onClick={()=>clickMe("faruk")}>clickme</button>
-    </div> */}
-      {/* <Event/> */}
-      {/* <UseStateHook/> */}
-      {/* <MapFunction/> */}
-      <ProductList products={products} />
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProductList products={products} deleteProduct={deleteProduct} />
+            }
+          />
+
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- my-react-app/src/components/ProductList.js
+++ my-react-app/src/components/ProductList.js
@@ -1,11 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
-const ProductList = ({ products }) => {
+// const ProductList = ({ products }) => {
+//     return (
+//         <div>
+//             <ul>
+//                 {products.map((product) => (
+//                 <li key={ product.id }> { product.title } - { product.price } </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+ 
+// export default ProductList
+
+
+const ProductList = ({ products, deleteProduct }) => {
     return (
         <div>
             <ul>
                 {products.map((product) => (
-                <li key={ product.id }> { product.title } - { product.price } </li>
+                <li key={ product.id }> { product.title } - { product.price } 
+                    <button onClick={ () => deleteProduct(product.id) }>Delete</button>
+                </li>
                 ))}
             </ul>
         </div>
